121c47f7085194338bc23ebff6257b3c828c2905
Cho-ga-nom <uipjo123@naver.com>
2022-11-05 14:51:14 +0900

    2022-11-05

---
+++ 2022-11-05/selfNumber.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void selfNumber(int);
+
+int main()
+{
+  int i = 1;
+
+  while (1)
+  {
+    selfNumber(i);
+    i++;
+  }
+}
+
+void selfNumber(int num)
+{
+}
