37b78ae748f4c4c41dc53010320f5aa2e8720b0a
Cho-ga-nom <uipjo123@naver.com>
2023-12-12 10:00:00 +0900

    commit

---
+++ 기타 문제 정리/현대5번.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+//	char input[100];
+//	int answer = 0;
+//	cin >> input;
+//	cout << answer;
+//
+//	return 0;
+//}
